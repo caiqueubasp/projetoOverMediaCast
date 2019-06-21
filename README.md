4b06166c6881019b7c95fc1e1c082eb2e61de27d
Caique Souza <caiqueubasp@gmail.com>
2019-06-21 04:22:40 -0300

    envio dos arquivos

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # projetoOverMediaCast
 projetoOverMediaCast
+
+
+
+https://www.4devs.com.br/gerador_de_texto_lorem_ipsum
+https://www.w3schools.com/cssref/css3_pr_animation-timing-function.asp
+https://daneden.github.io/animate.css/
+https://wowjs.uk/docs.html
+https://github.com/daneden/animate.css?files=1
+https://medium.com/code-prestige/anima%C3%A7%C3%B5es-com-wow-js-e-animate-css-5019ff0a97b9
+https://pt.stackoverflow.com/questions/114598/repetir-anima%C3%A7%C3%A3o-em-css
+https://pt.stackoverflow.com/questions/272153/anima%C3%A7%C3%A3o-de-imagem-com-efeito-pulse-e-brilho-em-css
+
--- main.js
+++ main.js
@@ -1,17 +1,62 @@
 
 
-let item1 = document.querySelector('#item1')
-let modal1 = document.querySelector('.modal1')
-let main = document.querySelector(".main-box")
-let btnModal = document.querySelector('#btn-modal')
+
+
+let main = document.querySelector(".main-box");
+
+let item1 = document.querySelector('.item1');
+let item2 = document.querySelector('.item2');
+let item3 = document.querySelector('.item3');
+let item4 = document.querySelector('.item4');
+
+let modal1 = document.querySelector('.modal-container1');
+let modal2 = document.querySelector('.modal-container2');
+let modal3 = document.querySelector('.modal-container3');
+let modal4 = document.querySelector('.modal-container4');
+
+let btnModal1 = document.querySelector('.btn-modal1');
+let btnModal2 = document.querySelector('.btn-modal2');
+let btnModal3 = document.querySelector('.btn-modal3');
+let btnModal4 = document.querySelector('.btn-modal4');
+
 
 item1.onclick = function(){
     modal1.style.display = "block";
-    main.style.background = "black";
     console.log("teste")
 }
 
-btnModal.onclick = function(){
+btnModal1.onclick = function(){
     modal1.style.display = "none";
-    main.style.background = "url('img/imagem-de-fundp.png')";
+    modal1.style.display = ""
+    
+}
+
+item2.onclick = function(){
+    modal2.style.display = "block";
+    console.log("teste")
+}
+
+btnModal2.onclick = function(){
+    modal2.style.display = "none";
+    
+}
+
+item3.onclick = function(){
+    modal3.style.display = "block";
+    console.log("teste")
+}
+
+btnModal3.onclick = function(){
+    modal3.style.display = "none";
+    
+}
+
+item4.onclick = function(){
+    modal4.style.display = "block";
+    console.log("teste")
+}
+
+btnModal4.onclick = function(){
+    modal4.style.display = "none";
+    
 }
